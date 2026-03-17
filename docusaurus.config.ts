89efe1f6c4a04151026ca6ad4c7cd04fcc92924f
Josue Rojas <josuerojas.rojas@gmail.com>
2026-03-16 22:22:49 -0400

    cname

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  url: 'https://1o1e.github.io',
+  url: 'https://1o13.com',
   baseUrl: '/',
 
   // GitHub pages deployment config.
